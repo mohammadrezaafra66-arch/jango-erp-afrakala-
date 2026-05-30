2965ac0d15f5581887884bfba31ffae796382e79
mohammadrezaafra66-arch <mohammadrezaafra66@gmail.com>
2026-05-30 16:32:00 +0330

    Add bot panel app config

---
+++ bot_panel/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class BotPanelConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "bot_panel"
+    verbose_name = "پنل ربات‌ها"
